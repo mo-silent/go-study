61de7dbe54cd3ee890217767a5631af69fe99da8
MoGD2018 <MoGD2018@gmail.com>
2022-04-21 10:56:49 +0800

    20220421: add select deadlock test

--- test/main.go
+++ test/main.go
@@ -1,32 +1,31 @@
-package main
-
-// import "C"
-
-func main() {
-	var ch chan struct{}
-	<-ch
-}
-
 // package main
 
-// import "sync"
+// // import "C"
 
 // func main() {
-// 	var wg sync.WaitGroup
-// 	foo := make(chan int)
-// 	bar := make(chan int)
-// 	closing := make(chan struct{})
-// 	wg.Add(1)
-// 	go func() {
-// 		defer wg.Done()
-// 		select {
-// 		case v := <-bar:
-// 			foo <- v
-// 		case <-closing:
-// 			println("closing")
-// 		}
-// 	}()
-// 	// bar <- 123
-// 	close(closing)
-// 	wg.Wait()
+// 	var ch chan struct{}
+// 	<-ch
 // }
+package main
+
+import "sync"
+
+func main() {
+	var wg sync.WaitGroup
+	foo := make(chan int)
+	bar := make(chan int)
+	closing := make(chan struct{})
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		select {
+		case v := <-bar:
+			foo <- v
+		case <-closing:
+			println("closing")
+		}
+	}()
+	// bar <- 123
+	close(closing)
+	wg.Wait()
+}
