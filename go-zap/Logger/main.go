4f616decd182e9011ec222bc0fc3c46657acbab2
MoGD2018 <MoGD2018@gmail.com>
2022-04-22 21:02:15 +0800

    20220422: add go log

---
+++ go-zap/Logger/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var logger *zap.Logger
+
+// var sugarLogger *zap.SugaredLogger
+
+func main() {
+	InitLogger()
+	defer logger.Sync()
+	simpleHttpGet("www.google.com")
+	simpleHttpGet("http://www.google.com")
+}
+
+func InitLogger() {
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+	logger = zap.New(core)
+	// sugarLogger := logger.Sugar()
+}
+
+func getEncoder() zapcore.Encoder {
+	return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+}
+
+func getLogWriter() zapcore.WriteSyncer {
+	file, _ := os.Create("./test.log")
+	return zapcore.AddSync(file)
+}
+
+func simpleHttpGet(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.Error(
+			"Error fetching url..",
+			zap.String("url", url),
+			zap.Error(err))
+	} else {
+		logger.Info("Success..",
+			zap.String("statusCode", resp.Status),
+			zap.String("url", url))
+		resp.Body.Close()
+	}
+}
