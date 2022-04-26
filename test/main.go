85ddeecafa17b97334fbe5bcd94ea28a6ac25b44
MoGD2018 <MoGD2018@gmail.com>
2022-04-26 14:54:57 +0800

    20220426: eidt test

--- test/main.go
+++ test/main.go
@@ -8,27 +8,32 @@
 // }
 package main
 
-import "sync"
+import "fmt"
 
 func main() {
-	var wg sync.WaitGroup
-	foo := make(chan int)
-	bar := make(chan int)
-	closing := make(chan struct{})
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		select {
-		case foo <- <-bar:
-			println("bar")
-		case <-closing:
-			println("closing")
-		}
-	}()
-	// bar <- 123
-	close(closing)
-	close(bar)
-	<-foo
-	close(foo)
-	wg.Wait()
+	// var wg sync.WaitGroup
+	// foo := make(chan int)
+	// bar := make(chan int)
+	// closing := make(chan struct{})
+	// wg.Add(1)
+	// go func() {
+	// 	defer wg.Done()
+	// 	select {
+	// 	case foo <- <-bar:
+	// 		println("bar")
+	// 	case <-closing:
+	// 		println("closing")
+	// 	}
+	// }()
+	// // bar <- 123
+	// close(closing)
+	// close(bar)
+	// <-foo
+	// close(foo)
+	// wg.Wait()
+	var ar = []byte{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
+	// var a []byte
+	a := ar[2:5]
+	a[0] = 'd'
+	fmt.Println(ar)
 }
